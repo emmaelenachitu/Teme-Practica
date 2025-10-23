004759730579364259ee1ffdb34785fc201cf01c
emmaelenachitu <emmaelena.chitu@gmail.com>
2025-10-23 12:44:13 +0300

    Add files via upload
    ex 1.2 tema 3

---
+++ Interup/GccApplication11/Debug/main.d
@@ -0,0 +1,46 @@
+main.d main.o: .././main.c \
+ e:\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h \
+ e:\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h \
+ e:\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
+ e:\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
+ e:\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ E:\Microchip\7.0\Packs\atmel\ATmega_DFP\1.7.374\include/avr/iom2560.h \
+ e:\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h \
+ e:\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h \
+ e:\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h \
+ e:\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h \
+ e:\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h \
+ e:\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h \
+ e:\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
+ e:\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
+ e:\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h
+
+e:\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
+
+e:\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h:
+
+e:\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+e:\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
+
+e:\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+E:\Microchip\7.0\Packs\atmel\ATmega_DFP\1.7.374\include/avr/iom2560.h:
+
+e:\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h:
+
+e:\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h:
+
+e:\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h:
+
+e:\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h:
+
+e:\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h:
+
+e:\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h:
+
+e:\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
+
+e:\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
+
+e:\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
---
+++ Interup/GccApplication11/main.c
@@ -0,0 +1,51 @@
+/*
+ * GccApplication11.c
+ * Author : emmae
+*/
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#define F_CPU 16000000UL
+#include <util/delay.h>
+#define baud 9600
+#define UBRRVAL ((F_CPU/16/baud) - 1)
+
+// Buffer pentru transmitere/recep?ie
+volatile char received_char = 0;  // stocheaz? caracterul primit
+
+// Initializare UART
+void uart_init(void){
+    UBRR0H = (unsigned char)(UBRRVAL >> 8);
+    UBRR0L = (unsigned char)UBRRVAL;
+    UCSR0B = (1 << TXEN0) | (1 << RXEN0) | (1 << RXCIE0); // TX, RX si RX Interrupt
+    UCSR0C = (1 << UCSZ01) | (1 << UCSZ00); // 8-bit data
+}
+
+// Transmitere caracter (polling)
+void uart_putc(char c){
+    while(!(UCSR0A & (1 << UDRE0)));
+    UDR0 = c;
+}
+
+// Transmitere sir
+void uart_puts(const char *s){
+    while(*s)
+        uart_putc(*s++);
+}
+
+// ISR pentru receptionare UART
+ISR(USART0_RX_vect){
+	received_char = UDR0;      // citeste caracterul din buffer
+	uart_putc(received_char);  // echo inapoi
+}
+
+
+int main(void){
+    uart_init();
+    sei();  // enable global interrupts
+
+    uart_puts("UART Reception Interrupt Example\r\n");
+
+    while(1){}
+    
+}
